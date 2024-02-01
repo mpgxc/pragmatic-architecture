c8b3c021fe18ec26625baf546171c3e0f1a67e6f
mpgxc <mpgx5.c@gmail.com>
2024-02-01 01:30:34 -0300

    feat: implementa rota simples de health-check

---
+++ src/infra/controllers/health.controller.ts
@@ -0,0 +1,37 @@
+import { LoggerInject, LoggerService } from '@mpgxc/logger';
+import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ApiTags } from '@nestjs/swagger';
+import {
+  HealthCheck,
+  HealthCheckService,
+  HttpHealthIndicator,
+} from '@nestjs/terminus';
+
+@ApiTags('Health')
+@Controller('health')
+export class HealthCheckController {
+  constructor(
+    @LoggerInject(HealthCheckController.name)
+    private readonly logger: LoggerService,
+    private readonly config: ConfigService,
+    private readonly health: HealthCheckService,
+    private readonly http: HttpHealthIndicator,
+  ) {}
+
+  @Get('/check')
+  @HealthCheck()
+  async handle() {
+    this.logger.log('Health check > triggered');
+
+    return this.health.check([
+      () => this.http.pingCheck('github', 'https://github.com'),
+      () => ({
+        root: {
+          status: 'up',
+          message: `API is up and running on <${this.config.get<string>('NODE_ENV').toUpperCase()}> stage`,
+        },
+      }),
+    ]);
+  }
+}
--- src/infra/infra.module.ts
+++ src/infra/infra.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TerminusModule } from '@nestjs/terminus';
 
 import { LoggerModule } from '@mpgxc/logger';
-import { RegisterEstablishment } from '@usecases/establishments/register-establishment';
-import { EstablishmentController } from './controllers/establishment.controller';
-import { DatabaseModule } from './database/database.module';
+import { HttpModule } from '@nestjs/axios';
 import { GetEstablishment } from '@usecases/establishments/get-establishment';
 import { ListEstablishments } from '@usecases/establishments/list-establishments';
+import { RegisterEstablishment } from '@usecases/establishments/register-establishment';
+import { EstablishmentController } from './controllers/establishment.controller';
+import { HealthCheckController } from './controllers/health.controller';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -17,8 +20,10 @@ import { ListEstablishments } from '@usecases/establishments/list-establishments
       isGlobal: true,
     }),
     DatabaseModule,
+    TerminusModule,
+    HttpModule,
   ],
   providers: [RegisterEstablishment, GetEstablishment, ListEstablishments],
-  controllers: [EstablishmentController],
+  controllers: [EstablishmentController, HealthCheckController],
 })
 export class InfraModule {}
