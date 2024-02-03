76b74ba1fc7e8bef99e681c57cd89c1b8d37941a
mpgxc <mpgx5.c@gmail.com>
2024-02-03 17:16:31 -0300

    feat(common/types): adiciona repositório genérico

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,3 +1,20 @@
+import { OptionalPromise } from '@common/logic';
+import { Pagination } from '@infra/database/dynamo/types';
+import { UUID } from 'node:crypto';
+
+export type OutputList<E> = {
+  Items: Entity<E>[];
+  LastEvaluatedKey: string;
+  Count: number;
+};
+
+export interface Repository<E, Filters = any> {
+  create(props: E): Promise<void>;
+  get(establishmentId: UUID): OptionalPromise<Entity<E>>;
+  list(pagination: Pagination, filters?: Filters): Promise<OutputList<E>>;
+  update(establishmentId: UUID, props: Partial<E>): Promise<void>;
+}
+
 export type Entity<Content = any> = {
   PK: string;
   SK: string;
