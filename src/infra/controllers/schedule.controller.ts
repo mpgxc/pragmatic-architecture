ef34eba5172547635805bd6786df28f5a3b35431
demmorou <sousadeusimar2@gmail.com>
2024-02-27 20:51:40 -0300

    fix: corrige tipagem swagger de register schedule

--- src/infra/controllers/schedule.controller.ts
+++ src/infra/controllers/schedule.controller.ts
@@ -1,4 +1,4 @@
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { RegisterSchedule } from '@usecases/schedule/register-schedule';
 import { CreateSchedule } from './validators/schedule';
@@ -9,6 +9,9 @@ export class ScheduleController {
   constructor(private readonly registerSchedule: RegisterSchedule) {}
 
   @HttpCode(HttpStatus.CREATED)
+  @ApiCreatedResponse({
+    description: 'The schedule has been successfully registered.',
+  })
   @Post()
   async create(@Body() payload: CreateSchedule) {
     const result = await this.registerSchedule.execute(payload);
